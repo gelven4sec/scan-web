e6c848cc8d17fcffa09b606b4476116607e62b58
sven-eliasen <jo.pettersen@pm.me>
2020-11-11 13:20:10 +0100

    added comments + refactoring

--- run.py
+++ run.py
@@ -3,68 +3,72 @@
 
 from bottle import Bottle, run, route, static_file, request, post
 import os
-from pythonping import ping
 from urllib.request import urlopen
 from urllib.parse import urlparse
 
 app = Bottle()
 
-#send main web page
+
+# send main web page
 @app.route('/')
 def server_static(filepath="index.html"):
-	return static_file(filepath, root='/home/pi/scan-web/public/')
+    return static_file(filepath, root='/home/pi/scan-web/public/')
 
-#send static files(css, js, img, etc...)
+
+# send static files(css, js, img, etc...)
 @app.route('/static/<filepath:path>')
 def server_static_file(filepath):
-	return static_file(filepath, root='/home/pi/scan-web/public/static/')
+    return static_file(filepath, root='/home/pi/scan-web/public/static/')
 
-#sqli injection scanner function called from AJAX on client
+
+# sqli injection scanner function called from AJAX on client
 @app.post('/process')
 def process_scan():
-	#get url from POST method
-	url = request.forms.get('url')
+    # get url from POST method
+    url = request.forms.get('url')
 
-	#parse input to get hostname
-	target = urlparse(url).hostname
-	#if value is None means the url input is the exact hostname
-	if target == None:
-		target = url
-		try:
-			code = urlopen("http://"+url).getcode()
-		except:
-			return 'Invalid target or offline : Code 1'
-	else:
-		#check if target is joinable
-		try:
-			code = urlopen(url).getcode()
-		except:
-			return 'Invalid target or offline: Code 2'
+    # parse input to get hostname
+    target = urlparse(url).hostname
+    # if value is None means the url input is the exact hostname
+    if target is None:
+        target = url
+        try:
+            code = urlopen("http://" + url).getcode()
+        except:
+            return 'Invalid target or offline : Code 1'
+    else:
+        # check if target is joinable
+        try:
+            code = urlopen(url).getcode()
+        except:
+            return 'Invalid target or offline: Code 2'
 
-	if code != 200:
-		return 'Invalid target or offline'
+    if code != 200:
+        return 'Invalid target or offline'
 
-	#start scan with sqlmap
-	os.system("sqlmap -u \'"+url+"\' --crawl=1 --batch --forms --technique=BE")
+    # start scan with sqlmap
+    os.system("sqlmap -u \'" + url + "\' --crawl=1 --batch --forms --technique=BE")
 
-	#DEBUG
-	#print("PATH : home/pi/.sqlmap/output/"+target+"/log")
+    # if this fail, sqlmap didn't found a form so it didn't even created the directory
+    try:
+        f = open("/root/.sqlmap/output/" + target + "/log", 'r')
+    except:
+        return 'Target not vulnerable to SQL Injection ! : Code 3'
 
-	try:
-		f = open("/root/.sqlmap/output/"+target+"/log", 'r')
-	except:
-		return 'Target not vulnerable to SQL Injection ! : Code 3'
+    # save content and close file
+    result = f.read()
+    f.close()
 
-	result = f.read()
-	f.close()
+    # clean sqlmap directory
+    os.system("sqlmap --purge")
 
-	#clean sqlmap directory
-	os.system("sqlmap --purge")
+    # if file content is empty, means no param were injectable
+    if len(result) == 0:
+        return 'Target not vulnerable to SQL Injection ! : Code 4'
 
-	if len(result) == 0:
-		return 'Target not vulnerable to SQL Injection ! : Code 4'
+    # clean sqlmap directory to cause result will be saved somewhere else
+    os.system("sqlmap --purge")
+    return result
 
-	os.system("sqlmap --purge")
-	return result
 
 run(app, host='0.0.0.0', port=2323, debug=True)
