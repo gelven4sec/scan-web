1418f88fca6a7bbfea9a44eaf185f44a74346cdd
Joakim Pettersen <37241392+sven-eliasen@users.noreply.github.com>
2020-12-29 16:49:06 +0100

    update js
    download function

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,5 +1,5 @@
 function process() {
-  let input = document.getElementById('input');
+  let input = document.getElementById('basic-url');
   let text = input.value.trim();
   console.log(text);
 
@@ -15,11 +15,14 @@ function process() {
          let response = request.responseText;
          let result = document.getElementById('result');
          result.innerHTML = response;
+         remove_gif();
+         enable_download();
 	     }
     };
 
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     request.send("url="+text);
+	load_gif()
 
     let result = document.getElementById('result');
     result.innerHTML = "Waiting for process...";
@@ -29,4 +32,43 @@ function process() {
     input.style.border = "2px solid red";
     alert("Not a valid url");
   }
+}
+
+function load_gif(){
+  var img = document.createElement("img");
+  img.src = "static/img/ajaxLoader.gif";
+  img.alt = "wait for scan...";
+	img.id = "gif_loader";
+
+  document.body.appendChild(img);
+}
+
+function remove_gif(){
+  let gif = document.getElementById("gif_loader");
+  gif.remove();
+}
+
+function download() {
+  let today = new Date();
+  let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  let filename = "report-"+date;
+
+  let textaera = document.getElementById("result");
+  let text = textaera.innerHTML;
+
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+function enable_download(){
+  let download = document.getElementById("download_btn");
+  download.removeAttribute("disabled");
 }
