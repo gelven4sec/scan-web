6887f3503fb8031f8d8544a09a4b14e67ab3f326
sven-eliasen <jo.pettersen@pm.me>
2020-12-29 22:44:13 +0100

    test scan forms 6

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import scan
 from urllib.parse import urlparse, urljoin
 from urllib.request import urlopen
 
@@ -102,13 +101,41 @@ def process_xss():
     # initialize an HTTP session
     session = HTMLSession()
 
-    # get all form tags
-    forms = scan.get_all_forms(url, session)
+    # GET request
+    res = session.get(url)
+
+    soup = BeautifulSoup(res.html.html, "html.parser")
+    forms = soup.find_all("form")
+
     # iterate over forms
     for i, form in enumerate(forms, start=1):
-        form_details = scan.get_form_details(form)
+        form_details = get_form_details(form)
         #print("=" * 50, f"form #{i}", "=" * 50)
         print(form_details)
 
+
+def get_form_details(form):
+    details = {}
+    # get the form action (requested URL)
+    action = form.attrs.get("action").lower()
+    # get the form method (POST, GET, DELETE, etc)
+    # if not specified, GET is the default in HTML
+    method = form.attrs.get("method", "get").lower()
+    # get all form inputs
+    inputs = []
+    for input_tag in form.find_all("input"):
+        # get type of input form control
+        input_type = input_tag.attrs.get("type", "text")
+        # get name attribute
+        input_name = input_tag.attrs.get("name")
+        # get the default value of that input tag
+        input_value = input_tag.attrs.get("value", "")
+        # add everything to that list
+        inputs.append({"type": input_type, "name": input_name, "value": input_value})
+    # put everything to the resulting dictionary
+    details["action"] = action
+    details["method"] = method
+    details["inputs"] = inputs
+    return details
 
 run(app, host='0.0.0.0', port=2323, debug=True)
