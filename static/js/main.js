5770415a76f67d610dc5c271534cd5fae3f78c56
sven-eliasen <jo.pettersen@pm.me>
2021-01-14 15:26:50 +0100

    right http request

--- static/js/main.js
+++ static/js/main.js
@@ -9,14 +9,14 @@ function process() {
   if (text.match(regex)) {
     //AJAX SQLi
     let request = new XMLHttpRequest();
-    request.open('POST', 'process');
+    request.open('POST', 'https://scan-web.octobyte.cloud/process');
     request.onreadystatechange = function(){
 	     if(request.readyState === 4){
          let response = request.responseText;
          let result = document.getElementById('result');
          result.innerHTML = "========================== SQLi ==========================\r\n" + response + "\r\nWaiting for XSS scan process...";
 
-         scan_xss(text);
+         //scan_xss(text);
 	     }
     };
 
@@ -78,7 +78,7 @@ function enable_download(){
 function scan_xss(text) {
   //AJAX XSS
   let request = new XMLHttpRequest();
-  request.open('POST', 'process_xss');
+  request.open('POST', 'https://scan-web.octobyte.cloud/process_xss');
   request.onreadystatechange = function(){
     if(request.readyState === 4){
       let response = request.responseText;
