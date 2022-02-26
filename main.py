01acb3ecc34c8271ffc973f7f024b8564a534d37
leo78450 <harpiemangue37@gmail.com>
2022-02-26 19:53:43 +0100

    The new and (best) version
    Co-authored-by: Joakim PETTERSEN <jpettersen@myges.fr>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+from queue import Empty
 import random
 from urllib.parse import urlparse, urljoin
 from urllib.request import urlopen
@@ -12,15 +13,14 @@ from bs4 import BeautifulSoup
 
 app = Bottle()
 
-
 # send main web page
-@app.route('/')
+@app.get('/')
 def server_static(filepath="index.html"):
     return static_file(filepath, root='public/')
 
 
 # send static files(css, js, img, etc...)
-@app.route('/static/<filepath:path>')
+@app.get('/static/<filepath:path>')
 def server_static_file(filepath):
     return static_file(filepath, root='public/static/')
 
@@ -35,24 +35,7 @@ def process_scan():
     # get url from POST method
     url = request.forms.get('url')
 
-    # parse input to get hostname
-    target = urlparse(url).hostname
-    # if value is None means the url input is the exact hostname
-    if target is None:
-        target = url
-        try:
-            code = urlopen(f"http://{url}").getcode()
-        except:
-            return 'Invalid target or offline : Code 1'
-    else:
-        # check if target is enable
-        try:
-            code = urlopen(url).getcode()
-        except:
-            return 'Invalid target or offline: Code 2'
-
-    if code != 200:
-        return 'Invalid target or offline'
+    target = verif(url)
 
     # start scan with sqlmap
     os.system("sqlmap -u \'" + url + "\' --crawl=1 --batch --forms --technique=E")
@@ -66,9 +49,6 @@ def process_scan():
     # save content and close file
     result = f.read()
     f.close()
-
-    # clean sqlmap directory
-    os.system("sqlmap --purge")
 
     # if file content is empty, means no param were injectable
     if len(result) == 0:
@@ -87,25 +67,8 @@ def process_xss():
 
     # get url from POST method
     url = request.forms.get('url')
-
-    # parse input to get hostname
-    target = urlparse(url).hostname
-    # if value is None means the url input is the exact hostname
-    if target is None:
-        target = url
-        try:
-            code = urlopen("http://" + url).getcode()
-        except:
-            return 'Invalid target or offline'
-    else:
-        # check if target is enable
-        try:
-            code = urlopen(url).getcode()
-        except:
-            return 'Invalid target or offline'
-
-    if code != 200:
-        return 'Invalid target or offline'
+    
+    target = verif(url)
 
     # initialize an HTTP session
     session = HTMLSession()
@@ -114,39 +77,72 @@ def process_xss():
     res = session.get(url)
 
     soup = BeautifulSoup(res.html.html, "html.parser")
-    form = soup.find_all("form")[0]
-    form_details = get_form_details(form)
 
-    num = str(random.randint(0, 100))
-    # the data body we want to submit
-    data = {}
-    for input_tag in form_details["inputs"]:
-        if input_tag["type"] == "text":
-            data[input_tag["name"]] = '<p id="' + num + '">scan_xss:' + num + '</p>'
-        elif input_tag["type"] == "hidden":
-            data[input_tag["name"]] = input_tag["value"]
+    result_id = []
 
-    # join the url with the action (form request URL)
-    url_target = urljoin(url, form_details["action"])
+    for form in soup.find_all("form"):
+        form_details = get_form_details(form)
 
-    res = session.post(url_target, data=data)
+        num = str(random.randint(0, 100))
+        # the data body we want to submit
+        data = {}
+        for input_tag in form_details["inputs"]:
+            if input_tag["type"] == "text":
+                data[input_tag["name"]] = '<p id="' + num + '">scan_xss:' + num + '</p>'
+            elif input_tag["type"] == "hidden":
+                data[input_tag["name"]] = input_tag["value"]
 
-    soup = BeautifulSoup(res.content, "html.parser")
-    for p in soup.find_all("p"):
-        if 'id' in p.attrs:
-            if p.attrs["id"] == num:
-                return "Target VULNERABLE to XSS injection !\r\nThe following tag has been injected:\r\n" + '<p id="' + num + '">scan_xss:' + num + '</p>'
+        # join the url with the action (form request URL)
+        url_target = urljoin(url, form_details["action"])
 
-    return "Target NOT VULNERABLE to XSS injection !"
+        if (form_details["method"] == "post"):
+            res = session.post(url_target, data=data)
+        else :
+            res = session.get(url_target, params=data) # ?name=caca&test=davy
+        soup = BeautifulSoup(res.content, "html.parser")
+        for p in soup.find_all("p"):
+            if 'id' in p.attrs:
+                if p.attrs["id"] == num:
+                    # TODO: retourner la list des id des formulaires vulnérables
+                    result_id += form_details["id"]
+                    #return "Target VULNERABLE to XSS injection !\r\nThe following tag has been injected:\r\n" + '<p id="' + num + '">scan_xss:' + num + '</p>'
 
+    if (len(result_id) == 0):
+        return "Target NOT VULNERABLE to XSS injection !"
+    else :
+        return "Target VULNERABLE to XSS injection !\r\n" + '\r\n'.join(result_id)
+
+def verif(url):
+    # parse input to get hostname
+    target = urlparse(url).hostname
+    # if value is None means the url input is the exact hostname
+    if target is None:
+        target = url
+        try:
+            code = urlopen(f"http://{url}").getcode()
+        except:
+            return 'Invalid target or offline : Code 1'
+    else:
+        # check if target is enable
+        try:
+            code = urlopen(url).getcode()
+        except:
+            return 'Invalid target or offline: Code 2'
+
+    if code != 200:
+        return 'Invalid target or offline'
+    return target
 
 def get_form_details(form):
     details = {}
+    id = form.attrs.get("id").lower()
     # get the form action (requested URL)
     action = form.attrs.get("action").lower()
     # get the form method (POST, GET, DELETE, etc)
-    # if not specified, GET is the default in HTML
-    method = form.attrs.get("method", "get").lower()
+    if (form.attrs.get("method", "get").lower() == Empty):
+        method = form.attrs.get("method", "post").lower()
+    else:
+        method = form.attrs.get("method", "get").lower()
     # get all form inputs
     inputs = []
     for input_tag in form.find_all("input"):
@@ -162,8 +158,8 @@ def get_form_details(form):
     details["action"] = action
     details["method"] = method
     details["inputs"] = inputs
+    details["id"] = id
     return details
-
 
 if __name__ == "__main__":
     run(app, host='0.0.0.0', port=2323, debug=True)
